0a3d824f460a063540575ceab6984c43391f9749
Trollface7272 <trollface7272@post.cz>
2021-12-08 14:13:11 +0100

    Error handling

--- Database/src/routes/auth.ts
+++ Database/src/routes/auth.ts
@@ -1,23 +1,29 @@
 import { Request, Response, Router } from "express";
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { SetOsuToken } from "../database/users";
 
 const router = Router()
 //https://osu.ppy.sh/oauth/authorize?redirect_uri=http://localhost:727/auth&response_type=code&client_id=11234&state=290850421487042560
 router.get("/", async (req: Request, res: Response) => {
     const { state, code } = req.query
-    const resp = (await axios.post("https://osu.ppy.sh/oauth/token", {
+    const resp = await axios.post("https://osu.ppy.sh/oauth/token", {
         client_id: process.env.OSUID,
         client_secret: process.env.OSU,
         redirect_uri: process.env.OSUURL,
         code,
         grant_type: "authorization_code"
-    })).data
+    }).catch((err: AxiosError) => {
+        console.log(err)
+        return
+    })
+    if (!resp) return res.status(500).send()
+    
+    const rawData = resp.data
     const data = {
-        tokenType: resp.token_type,
-        expireDate: new Date(resp.expires_in + Date.now()),
-        token: resp.access_token,
-        refresh: resp.refresh_token
+        tokenType: rawData.token_type,
+        expireDate: new Date(rawData.expires_in + Date.now()),
+        token: rawData.access_token,
+        refresh: rawData.refresh_token
     }
 
     SetOsuToken(state as string, data, req.headers["cf-connecting-ip"] as string)
